5fabd564def273cbd186774eb8967fa5810f1434
Worll <kvedarasarnas@gmail.com>
2018-12-10 15:34:26 +0200

    commit

---
+++ IS_CS/Controllers/IrenginysController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IS_CS.Controllers
+{
+    public class IrenginysController : Controller
+    {
+        // GET: Irenginys
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
