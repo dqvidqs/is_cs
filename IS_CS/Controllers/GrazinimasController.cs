38fa8fcbade1e40f46a2ca125f59d64bfe6906a9
Karoliina97 <karolina.endrikaityte@ktu.edu>
2018-12-10 20:10:40 +0200

    Dalis grąžinimo posistemės

---
+++ IS_CS/Controllers/GrazinimasController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IS_CS.Models;
+
+namespace IS_CS.Controllers
+{
+    public class GrazinimasController : Controller
+    {
+        private is_kpEntities1 _entities = new is_kpEntities1();
+
+        public ActionResult prekiu_grazinimas()
+        {
+            return View(_entities.Grazinimo_priezastis.ToList());
+        }
+
+        public ActionResult grazinimo_formos()
+        {
+            return View(_entities.Uzsakymas.ToList());
+        }
+
+        public ActionResult israsas()
+        {
+            return View(_entities.Uzsakymas.ToList());
+        }
+
+        public ActionResult grazinimo_patvirtinimas()
+        {
+            return View(_entities.Patvirtinimas.ToList());
+        }
+    }
+}
