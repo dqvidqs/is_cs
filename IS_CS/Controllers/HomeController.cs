9b95d4b994dd29e1a785f823bfa4aa4c9d756d16
dqvidqs <dqvidqs@gmail.com>
2018-12-10 12:42:48 +0200

    Add files via upload

---
+++ IS_CS/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IS_CS.Models;
+
+namespace IS_CS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult prekiu_grazinimas()
+        {
+            return View();
+        }
+        public ActionResult grazinimo_formos()
+        {
+            return View();
+        }
+        public ActionResult israsas()
+        {
+            return View();
+        }
+        public ActionResult grazinimo_patvirtinimas()
+        {
+            return View();
+        }
+        public ActionResult krepselio_perziura()
+        {
+            return View();
+        }
+        public ActionResult pristatymo_budai()
+        {
+            return View();
+        }
+        public ActionResult apmokejimo_budai()
+        {
+            return View();
+        }
+
+        public ActionResult registruoti_irengini()
+        {
+            return View();
+        }
+        public ActionResult irenginio_ataskaita()
+        {
+            return View();
+        }
+        public ActionResult kliento_informacija()
+        {
+            return View();
+        }
+        public ActionResult gedimu_istorija()
+        {
+            return View();
+        }
+        public ActionResult About() {
+            return View();
+        }
+
+        public ActionResult prekiu_sarasas()
+        {
+            return View();
+        }
+        public ActionResult saskaitu_sarasas()
+        {
+            return View();
+        }
+        public ActionResult preke_keitimas()
+        {
+            return View();
+        }
+        public ActionResult preke_nauja()
+        {
+            return View();
+        }
+    }
+}
---
+++ IS_CS/Controllers/PrekeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IS_CS.Models;
+
+namespace IS_CS.Controllers
+{
+    public class PrekeController : Controller
+    {
+        //-----------------------------------------------
+        private is_kpEntities1 _entities = new is_kpEntities1();
+        // GET: preke
+        public ActionResult preke_List()
+        {
+            return View(_entities.Prekes.ToList());
+        }
+        
+        public ActionResult preke_Edit(int id)
+        {
+            Preke preke = _entities.Prekes.Find(id);
+            if (preke == null)
+            {
+                return HttpNotFound();
+            }
+            return View(preke);
+        }
+        public ActionResult preke_Detail(int id)
+        {
+            Preke preke = _entities.Prekes.Find(id);
+            if (preke == null)
+            {
+                return HttpNotFound();
+            }
+            return View(preke);
+        }
+        public ActionResult preke_Create()
+        {
+            return View();
+        }
+    }
+}
