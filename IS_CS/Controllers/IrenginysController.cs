d33e938aa40ec879877cd23a9b65f867531be14b
Worll <kvedarasarnas@gmail.com>
2018-12-10 18:01:40 +0200

    controlerio commitas

--- IS_CS/Controllers/IrenginysController.cs
+++ IS_CS/Controllers/IrenginysController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IS_CS.Models;
 
 namespace IS_CS.Controllers
 {
     public class IrenginysController : Controller
     {
+        private is_kpEntities1 _entities = new is_kpEntities1();
         // GET: Irenginys
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult preke_List()
+        {
+            return View(_entities.Prekes.ToList());
+        }
+
     }
 }
