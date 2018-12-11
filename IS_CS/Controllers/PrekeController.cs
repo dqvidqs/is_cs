b8d9a03bc6aab1f600a813a1ae1eff0b3ef6d146
dqvidqs <dqvidqs@gmail.com>
2018-12-11 11:55:08 +0200

    Add files via upload

--- IS_CS/Controllers/PrekeController.cs
+++ IS_CS/Controllers/PrekeController.cs
@@ -15,7 +15,7 @@ namespace IS_CS.Controllers
         // GET: preke
         public ActionResult preke_List()
         {
-            
+
             int i = 0;
             var prekes = db.Prekes.ToList();
             foreach (var preke in prekes)
@@ -31,7 +31,7 @@ namespace IS_CS.Controllers
             }
             return View(prekes);
         }
-        
+
         public ActionResult preke_Edit(int id)
         {
             Preke preke = db.Prekes.Find(id);
@@ -111,6 +111,108 @@ namespace IS_CS.Controllers
             db.Prekes.Remove(preke);
             db.SaveChanges();
             return RedirectToAction("preke_List");
+        }
+        [HttpPost]
+        public ActionResult preke_List(string pavad, string model,
+            decimal bar0, decimal bar1, decimal price0, decimal price1,
+            string kt1, string kt2, string kt3)
+        {
+            int i = 0;
+            var prekes = db.Prekes.ToList();
+            foreach (var preke in prekes)
+            {
+                foreach (var tipas in db.Kiekio_tipas)
+                {
+                    if (preke.kiekio_tipas == tipas.id_Kiekio_tipas)
+                    {
+                        prekes.ElementAt(i).tipas = tipas.name;
+                    }
+                }
+                i++;
+            }
+            //PAVADINIMU
+            if (pavad != "")
+            {
+                for (i = 0; i < prekes.Count; i++)
+                {
+                    if (prekes.ElementAt(i).pavadinimas.ToLower() != pavad.ToLower())
+                    {
+                        prekes.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            //MODELIU
+            if (model != "")
+            {
+                for (i = 0; i < prekes.Count; i++)
+                {
+                    if (prekes.ElementAt(i).modelis.ToLower() != model.ToLower())
+                    {
+                        prekes.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            //BAR KODAI
+            if (bar0 != 0 && bar1 != 0)
+            {
+                for (i = 0; i < prekes.Count; i++)
+                {
+                    var temp = prekes.ElementAt(i).bar_kodas;
+                    if (bar0 > temp || temp > bar1)
+                    {
+                        prekes.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            //Kaina       
+            if (price0 != 0 && price1 != 0)
+            {
+                for (i = 0; i < prekes.Count; i++)
+                {
+                    var temp = Convert.ToDecimal(prekes.ElementAt(i).pardavimo_kaina);
+                    if (price0 > temp || temp > price1)
+                    {
+                        prekes.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            List<Preke> temp_list = new List<Preke>();
+            if (kt1 == "true")
+            {
+                for (i = 0; i < prekes.Count; i++)
+                {
+                    if (prekes.ElementAt(i).kiekio_tipas == 1)
+                    {
+                        temp_list.Add(prekes.ElementAt(i));
+                    }
+                }
+            }
+            if (kt2 == "true")
+            {
+                for (i = 0; i < prekes.Count; i++)
+                {
+                    if (prekes.ElementAt(i).kiekio_tipas == 2)
+                    {
+                        temp_list.Add(prekes.ElementAt(i));
+                    }
+                }
+            }
+            if (kt3 == "true")
+            {
+                for (i = 0; i < prekes.Count; i++)
+                {
+                    if (prekes.ElementAt(i).kiekio_tipas == 3 )
+                    {
+                        temp_list.Add(prekes.ElementAt(i)); 
+                    }
+                }
+            }
+            prekes = temp_list;
+            return View(prekes);
         }
     }
 }
